bc83e3e269debd680fe77fa3a316bd1c3561b3c6
andrewbastian <andrew.bastian@gmail.com>
2019-10-05 12:06:11 -0700

    ~MVP still need to get the followers data working 100%. More code needed

---
+++ my-user-card/src/Components/UserCard.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
+
+
+
+const UserCard = (props) => {
+  // console.log(props.myData.name)
+    const classes = useStyles();
+        return (
+          <Card className={classes.card}>
+
+
+
+                  <h1>{props.myData.name}</h1>
+
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="240"
+                    image={props.myData.avatar_url}
+                    title = "profile pic" />
+                  <CardContent>
+                     <Typography gutterBottom variant="h5" component="h2">
+                  {props.myData.login}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                  {props.myData.location}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                  {props.myData.bio}
+                  </Typography>
+                  </CardContent>
+                  <Button>{props.myData.followers} Followers</Button>
+
+
+
+          </Card>
+        )
+      }
+
+
+
+export default UserCard;
